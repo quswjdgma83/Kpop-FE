161ee41db9bfdfa5883872137dd7e48b6ff3c733
JeongHeum Byun <quswjdgma83@gmail.com>
2024-01-24 21:49:54 +0900

    chore: 이미지, svg(#7)

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -2,10 +2,12 @@ import IconAdd from './add';
 import IconCancel from './cancel';
 import IconEyeOpen from './eye_open';
 import IconHeart from './heart';
+import IconChecked from './checked';
 
 export {
   IconAdd,
   IconCancel,
   IconEyeOpen,
-  IconHeart
+  IconHeart,
+  IconChecked
 }
