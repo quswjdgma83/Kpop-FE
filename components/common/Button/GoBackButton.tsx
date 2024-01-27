4f756a37513163735f5d30f78d2d7abdb83b9422
ijaesin <ijaesinii@gmail.com>
2024-01-27 16:40:22 +0900

    feat: GoBackButton 기능 추가
    history.back() 기능 추가

--- components/common/Button/GoBackButton.tsx
+++ components/common/Button/GoBackButton.tsx
@@ -1,11 +1,19 @@
+'use client';
+
+import { useCallback } from 'react';
 import { Button } from '@nextui-org/react';
 import { IconArrowLeft } from '@/public/svgs';
 
 export default function GoBackButton() {
+  const onClick = useCallback(() => {
+    history.back();
+  }, []);
+
   return (
     <Button
       startContent={<IconArrowLeft />}
       className="bg-transparent text-white text-base p-0"
+      onClick={onClick}
     >
       뒤로가기
     </Button>
