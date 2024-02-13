9f6affc4aea050f0ad884290f7f1ad735530bef4
ijaesin <ijaesinii@gmail.com>
2024-02-13 11:47:24 +0900

    feat: 배너 컴포넌트 개발
    Related to: #8

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import { Banner } from '@/containers/home';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex flex-col gap-12">
+      <div>
+        <Banner />
+      </div>
+
+      <h2 className="font-semibold text-3xl">
+        K-Goods에서는 모든 아티스트의 굿즈 정보를 수집하고 있어요
+      </h2>
     </div>
   );
 }
