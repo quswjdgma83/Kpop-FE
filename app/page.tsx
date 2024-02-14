1b97c5bded2fa6e08b818f3b2b989fb701aca411
ijaesin <ijaesinii@gmail.com>
2024-02-14 11:03:01 +0900

    feat: 메인 페이지 CSS 수정
    Related to: #8

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,21 @@ export default function Home() {
         <Banner />
       </div>
 
-      <h2 className="font-semibold text-3xl">
-        K-Goods에서는 모든 아티스트의 굿즈 정보를 수집하고 있어요
-      </h2>
+      <div>
+        <h2 className="font-semibold text-3xl">
+          K-Goods에서는 모든 아티스트의 굿즈 정보를 수집하고 있어요
+        </h2>
+      </div>
 
-      <div className="flex flex-col gap-7">
+      <div className="flex flex-col gap-5">
         <AgencyList />
         <ArtistList />
+      </div>
+
+      <div>
+        <h2 className="font-semibold text-3xl">
+          내가 선택한 아티스트 굿즈 정보를 확인해보세요!
+        </h2>
       </div>
     </div>
   );
