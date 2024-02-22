69df7c94e65e6b4c617148b31be265386f709431
JeongHeum Byun <quswjdgma83@gmail.com>
2024-02-22 16:09:51 +0900

    feat: 카카오맵 크기조절 props 추가

--- components/map/KakaoMap/KakaoMap.tsx
+++ components/map/KakaoMap/KakaoMap.tsx
@@ -1,15 +1,22 @@
 import Script from 'next/script';
 import { Map } from 'react-kakao-maps-sdk';
 
+interface KakaoMapProps {
+  height: string;
+}
+
 const KAKAO_SDK_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_APP_JS_KEY}&autoload=false`;
 
-const KakaoMap = () => {
+const KakaoMap = ({ height }: KakaoMapProps) => {
   return (
     <>
       <Script src={KAKAO_SDK_URL} strategy="beforeInteractive" />
       <Map
         center={{ lat: 33.450701, lng: 126.570667 }}
-        style={{ width: '904px', height: '2754px' }}
+        style={{
+          width: '904px',
+          height: height,
+        }}
       ></Map>
     </>
   );
