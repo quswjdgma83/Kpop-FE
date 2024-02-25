ab9f842515c44888191332faefea5209313bc848
JeongHeum Byun <quswjdgma83@gmail.com>
2024-02-25 17:07:22 +0900

    feat: 포맷함수 utils로 분리

--- app/events/[id]/page.tsx
+++ app/events/[id]/page.tsx
@@ -14,6 +14,7 @@ import {
   IconSend,
 } from '@/public/svgs';
 import Link from 'next/link';
+import { formatEventDates, formatTimeRange } from '@/utils/formater/format';
 
 export default function EventsDetail() {
   const [isOpen, setIsOpen] = useState(false);
@@ -58,32 +59,6 @@ export default function EventsDetail() {
   const handleToggleAccordion = () => {
     setIsOpen(!isOpen);
   };
-
-  const formatEventDates = (startDate: string, endDate: string) => {
-    //날짜 포맷 함수
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-
-    const format = (date: Date) =>
-      `${date.getFullYear().toString().slice(-2)}.${(date.getMonth() + 1)
-        .toString()
-        .padStart(2, '0')}.${date.getDate().toString().padStart(2, '0')}`;
-
-    return `${format(start)} - ${format(end).slice(-5)}`;
-  };
-
-  function formatTimeRange(startTime: string, endTime: string): string {
-    // 시간 포맷 함수
-    const formatTime = (timeString: string) => {
-      const time = new Date(`1970-01-01T${timeString}`);
-      return new Intl.DateTimeFormat('ko-KR', {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false,
-      }).format(time);
-    };
-    return `${formatTime(startTime)} ~ ${formatTime(endTime)}`;
-  }
 
   return (
     <div className="w-full flex">
