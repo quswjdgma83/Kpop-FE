85a1ec821d1d06db0193df7696a1889672bcbaf8
ijaesin <ijaesinii@gmail.com>
2024-03-03 17:14:36 +0900

    feat: 아티스트 페이지 검색폼 구현
    IconSearch 아이콘 색상 변경 가능하도록 수정
    
    Related to: #19

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,11 +1,17 @@
 import { ethnocentric } from '@/public/fonts/fonts';
+import { IconSearch } from '@/public/svgs';
+import { Input } from '@nextui-org/react';
 
 export default function Artists() {
   return (
-    <section>
-      <p className={`${ethnocentric.className} text-4xl flex justify-center`}>
-        Artist
-      </p>
+    <section className="flex flex-col items-center gap-8">
+      <Input
+        placeholder="좋아하는 아이돌의 다양한 정보를 검색해보세요!"
+        radius="full"
+        endContent={<IconSearch color="#006FEE" />}
+        className="w-[500px] h-[60px] "
+      />
+      <p className={`${ethnocentric.className} text-4xl `}>Artist</p>
     </section>
   );
 }
