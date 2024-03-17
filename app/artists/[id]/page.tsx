9ed6dc359e676ff8ccb3ae21494b64e5839558cb
ijaesin <ijaesinii@gmail.com>
2024-03-17 20:41:10 +0900

    feat: 아티스트 상세 정보 구현
    Related to: #19

--- app/artists/[id]/page.tsx
+++ app/artists/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@nextui-org/react';
 import Image from 'next/image';
+import { ethnocentric } from '@/public/fonts/fonts';
 
 export default function ArtistDetail({
   params: { id },
@@ -16,7 +17,7 @@ export default function ArtistDetail({
         className="w-[1280px] h-[640px] object-cover"
       />
 
-      <section className="flex flex-col items-center -mt-[75px] gap-[10px]">
+      <section className="flex flex-col items-center -mt-[75px] gap-[10px] mb-[100px]">
         <Image
           src={`/mocks/artist/detail/artist_${id}_logo.png`}
           width={150}
@@ -39,6 +40,45 @@ export default function ArtistDetail({
         >
           팔로우
         </Button>
+      </section>
+
+      <section className="flex flex-col gap-[30px] w-[720px] mb-[100px]">
+        <h2
+          className={`${ethnocentric.className} text-4xl border-t-[10px] border-[#0A60FF] max-w-fit`}
+        >
+          About {id.toUpperCase()}
+        </h2>
+
+        <div className="text-xl">
+          <p>
+            RIIZE(라이즈)는 ‘Rise & Realize’를 더해 만든 이름으로, 함께 성장하고
+            꿈을 실현해 나아가는 팀이라는 의미다.
+          </p>
+
+          <br />
+
+          <p>
+            각자의 개성을 지닌 일곱 명의 멤버가 하나의 팀으로서 쌓아가는
+            리얼타임 오디세이(성장사)를 바탕으로, RIIZE만의 감정을 표현하는
+            독자적 음악 스타일인 Emotional Pop을 선사한다.{' '}
+          </p>
+
+          <br />
+
+          <p>
+            RIIZE는 일상의 모든 경험에서 영감을 얻는 Emotional Pop Rookie가
+            성장해, 세상 모두의 공감을 불러일으키는 Emotional Pop Artist로서의
+            꿈을 실현하겠다는 포부를 가지고 있다.
+          </p>
+        </div>
+      </section>
+
+      <section>
+        <h2
+          className={`${ethnocentric.className} text-4xl border-t-[10px] border-[#0A60FF] max-w-fit`}
+        >
+          Member
+        </h2>
       </section>
     </div>
   );
