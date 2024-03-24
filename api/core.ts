7d0d1057750bbf0df9d562a3e72fcfe226378dd4
ijaesin <ijaesinii@gmail.com>
2024-03-24 17:52:52 +0900

    feat: api사용에 활용될 core.ts 작성
    Related to: #27

--- api/core.ts
+++ api/core.ts
@@ -8,3 +8,59 @@ import axios, {
 } from 'axios';
 import { getCookie } from 'cookies-next';
 import qs from 'qs';
+import { ACCESS_TOKEN, HTTP_METHODS } from '@/constants';
+import { BaseResponse } from '@/types';
+
+const axiosInstance: AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+axios.defaults.paramsSerializer = (params) => {
+  return qs.stringify(params);
+};
+
+axiosInstance.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig) => {
+    const accessToken = getCookie(ACCESS_TOKEN)!;
+
+    if (!accessToken) {
+      return config;
+    }
+    config.headers.Authorization = `Bearer ${accessToken}`;
+    return config;
+  },
+  (error: AxiosError) => {
+    Promise.reject(error);
+  },
+);
+
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => response.data,
+  async (error: AxiosError) => {
+    if (!error.response) {
+      console.error('에러 응답이 없습니다.');
+      return Promise.reject(error);
+    }
+
+    console.error('에러가 발생했습니다.');
+    return Promise.reject(error);
+  },
+);
+
+const createApiMethod =
+  (instance: AxiosInstance, method: Method) =>
+  <T>(config: AxiosRequestConfig): Promise<BaseResponse<T>> =>
+    instance({ ...config, method });
+
+
+export default {
+  get: createApiMethod(axiosInstance, HTTP_METHODS.GET),
+  post: createApiMethod(axiosInstance, HTTP_METHODS.POST),
+  patch: createApiMethod(axiosInstance, HTTP_METHODS.PATCH),
+  put: createApiMethod(axiosInstance, HTTP_METHODS.PUT),
+  delete: createApiMethod(axiosInstance, HTTP_METHODS.DELETE),
+};
