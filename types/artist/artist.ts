7506e1c020d028605bf95956ce2f39df292d0198
ijaesin <ijaesinii@gmail.com>
2024-04-14 13:26:02 +0900

    feat: 아티스트 리스트 조회 api response 타입 정의
    ArtistListProps, PageInfo, ArtistListResponse
    
    Related to: #38

--- types/artist/artist.ts
+++ types/artist/artist.ts
@@ -1,3 +1,29 @@
+export interface ArtistListProps {
+  regDt: string;
+  modDt: string;
+  artistId: number;
+  artistCd: string;
+  artistName: string;
+  comment: string;
+  followCnt: number;
+  gender: string;
+  unitYN: string | null;
+  delYN: string;
+  regId: string;
+}
+
+export interface PageInfo {
+  page: number;
+  size: number;
+  totalElements: number;
+  totalPages: number;
+}
+
+export interface ArtistListResponse {
+  data: ArtistListProps[]
+  pageInfo: PageInfo;
+}
+
 export interface ArtistsContainerProps {
   list: {
     imgSrc: string;
