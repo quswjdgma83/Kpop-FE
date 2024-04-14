a5e93535c2f1868c2c4ceff0e56bfc1aafec4bf6
ijaesin <ijaesinii@gmail.com>
2024-04-14 22:34:11 +0900

    feat: 아티스트 관련 api 연결 함수 정의
    아티스트 전체 조회, 아티스트 상세 조회, 아티스트 등록,
    아티스트 수정, 아티스트 삭제, 아티스트 검색 api 연결
    
    Related to: #38

--- api/artist/apis.ts
+++ api/artist/apis.ts
@@ -1,4 +1,4 @@
-import { ArtistListResponse } from '@/types/artist';
+import { ArtistDetailResponse, ArtistListResponse, ArtistSearchResponse } from '@/types/artist';
 import http from '..';
 
 // eslint-disable-next-line import/prefer-default-export
@@ -8,4 +8,42 @@ export const getArtistList = ({pageParam}: {pageParam: number;}) =>
     params: {page: pageParam, size: 16}
   })
 
-  
+export const getArtistDetail = ({artistId}: {artistId: string}) =>
+  http.get<ArtistDetailResponse>({
+    url: `/artist/one/${artistId}`,
+  })
+
+export const createArtist = ({agencyId, artistCd, artistName, comment,gender, unitYN}: {agencyId: string; artistCd: string; artistName: string; comment: string; gender: string; unitYN: string | null;}) =>
+  http.post<ArtistDetailResponse>({
+    url: `/artist/${agencyId}`,
+    data: {
+      artistCd,
+      artistName,
+      comment,
+      gender,
+      unitYN
+    }
+  })
+
+export const modifyArtist = ({artistId, artistCd, artistName, comment,gender, unitYN}: {artistId: number; artistCd: string; artistName: string; comment: string; gender: string; unitYN: string | null;}) =>
+  http.put<ArtistDetailResponse>({
+    url: `/artist/${artistId}`,
+    data: {
+      artistCd,
+      artistName,
+      comment,
+      gender,
+      unitYN
+    }
+  })
+
+export const deleteArtist = ({artistId}: {artistId: number}) =>
+  http.delete({
+    url: `/artist/${artistId}`
+  })
+
+export const getSearchArtist = ({artistName}: {artistName: string}) =>
+  http.get<ArtistSearchResponse>({
+    url: '/artist/search',
+    params: {artistName}
+  })
--- api/artist/queries.ts
+++ api/artist/queries.ts
@@ -1,17 +1,46 @@
 'use client';
 
-import { useSuspenseInfiniteQuery } from '@tanstack/react-query';
-import { getArtistList } from './apis';
+import { useSuspenseInfiniteQuery, useQuery, useMutation, QueryClient } from '@tanstack/react-query';
+import { createArtist, deleteArtist, getArtistDetail, getArtistList, getSearchArtist, modifyArtist } from './apis';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useGetArtistList = () =>
   useSuspenseInfiniteQuery({
     queryKey: ['/artist/list'],
-    queryFn: ({pageParam}: {pageParam: number}) => getArtistList({pageParam}),
-     initialPageParam: 1,
+    queryFn: ({pageParam}: {pageParam: number}) => getArtistList({pageParam}), initialPageParam: 1,
     getNextPageParam: ({ data }) => {
       const { pageInfo } = data || {};
       const { totalPages, page } = pageInfo || {};
       return page === totalPages ? undefined : page + 1;
     },
+  })
+
+export const useGetArtistDetail = ({artistId}: {artistId: string;}) =>
+  useQuery({
+    queryKey: ['/artist/one', artistId],
+    queryFn: () => getArtistDetail({artistId})
+  })
+
+export const useCreateArtist = ({agencyId, artistCd, artistName, comment,gender, unitYN, queryClient}: {agencyId: string; artistCd: string; artistName: string; comment: string; gender: string; unitYN: string | null; queryClient: QueryClient}) =>
+  useMutation({
+    mutationFn: () => createArtist({agencyId, artistCd, artistName, comment,gender, unitYN}),
+    onSuccess: () => queryClient.invalidateQueries({queryKey: ['/artist/list']})
+  })
+
+export const useModifyArtist = ({artistId, artistCd, artistName, comment,gender, unitYN, queryClient}: {artistId: number; artistCd: string; artistName: string; comment: string; gender: string; unitYN: string | null; queryClient: QueryClient}) =>
+  useMutation({
+    mutationFn: () => modifyArtist({artistId, artistCd, artistName, comment,gender, unitYN}),
+    onSuccess: () => queryClient.invalidateQueries({queryKey: ['/artist/one', artistId]})
+  })
+
+export const useDeleteArtist = ({artistId, queryClient}: {artistId: number;  queryClient: QueryClient}) =>
+  useMutation({
+    mutationFn: () => deleteArtist({artistId}),
+    onSuccess: () => queryClient.invalidateQueries({queryKey: ['/artist/list']})
+  })
+
+export const useGetSearchArtist = ({artistName}: {artistName: string}) =>
+  useQuery({
+    queryKey: ['/artist/search'],
+    queryFn: () => getSearchArtist({artistName})
   })
