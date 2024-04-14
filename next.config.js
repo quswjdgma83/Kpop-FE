f813a93c7dd6b85d38c4c81921e5ce90375a64c9
ijaesin <ijaesinii@gmail.com>
2024-04-14 12:27:32 +0900

    chore: eslint airbnb 오류 해결을 위한 패키지 추가
    eslint-config-airbnb
    eslint-config-airbnb-typescript
    
    Related to: #42

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  // output: 'export',
 };
 
 module.exports = nextConfig;
