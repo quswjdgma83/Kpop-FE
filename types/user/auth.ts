fbb535a9525710d73b660582c7e76e3bbf196200
ijaesin <ijaesinii@gmail.com>
2024-04-28 15:43:21 +0900

    feat: auth 관련 api response 타입 정의
    Related to: #44

---
+++ types/user/auth.ts
@@ -0,0 +1,9 @@
+export interface SignUpResponse {
+  userId: number;
+  userName: string;
+  userEmail: string;
+  nickname: string;
+  userType: string;
+  socialType: string;
+  socialId: string;
+}
