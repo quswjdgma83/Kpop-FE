f2e136a11399fa6d42ef2cd70794305b04ad7a5a
JeongHeum Byun <quswjdgma83@gmail.com>
2024-05-22 00:15:59 +0900

    feat: 로그인핸들러 테스트위해 붙임(#48)

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,15 +1,50 @@
 'use client';
 
 import { useState, useCallback } from 'react';
+import { setCookies } from 'cookies-next';
+
 import { Input, Button } from '@nextui-org/react';
+import { useRecoilState } from 'recoil';
 import { IconEyeClosed, IconEyeOpen } from '@/public/svgs';
+import { postLogin } from '@/api/auth';
+import authState from '@/store/auth';
 
 export default function Signin() {
   const [isVisible, setIsVisible] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [auth, setAuth] = useRecoilState(authState);
 
   const toggleSwitch = useCallback(() => {
     setIsVisible(!isVisible);
   }, [isVisible]);
+
+  const handleLogin = async () => {
+    try {
+      const response = await postLogin({ userEmail: email, password });
+      // const accessToken = response.data?.loginUser.accessToken;
+      if (response.statusCode === 200) {
+        console.log(response.headers['authorization']);
+
+        if (response.statusCode === 200 && response.message === '로그인 성공') {
+          // const accessToken = response.headers['authorization'];
+          // const refreshToken = response.headers['authorization-refresh'];
+          // console.log(accessToken);
+          // setCookies('accessToken', accessToken);
+          // setCookies('refreshToken', refreshToken);
+          console.log(response);
+          setAuth({
+            isAuthenticated: true,
+            userEmail: email,
+          });
+        }
+      } else {
+        console.error('Login failed:', response.message);
+      }
+    } catch (error) {
+      console.error('Login failed:', error);
+    }
+  };
 
   return (
     <section className="flex flex-col items-center gap-8 my-10 ">
@@ -20,6 +55,8 @@ export default function Signin() {
         <Input
           type="email"
           placeholder="이메일 계정"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
           classNames={{
             inputWrapper: ['bg-component'],
           }}
@@ -31,8 +68,10 @@ export default function Signin() {
         <h3 className="font-semibold text-inactive">비밀번호</h3>
         <div className="flex flex-col gap-3">
           <Input
-            type={isVisible ? 'string' : 'password'}
+            type={isVisible ? 'text' : 'password'}
             placeholder="비밀번호 입력"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             classNames={{
               inputWrapper: ['bg-component'],
             }}
@@ -42,14 +81,17 @@ export default function Signin() {
                 className="bg-transparent"
                 onClick={toggleSwitch}
               >
-                {isVisible ? <IconEyeClosed /> : <IconEyeOpen password />}
+                {isVisible ? <IconEyeClosed /> : <IconEyeOpen />}
               </Button>
             }
           />
         </div>
       </div>
 
-      <Button className="w-[480px] bg-themeBlue/20 border-themeBlue text-inactive h-14">
+      <Button
+        className="w-[480px] bg-themeBlue/20 border-themeBlue text-inactive h-14"
+        onClick={handleLogin}
+      >
         이메일 로그인하기
       </Button>
     </section>
